33ef90d53ef9cfdf1676cb99d2806ecff262bade
Lokesh <Lokeshkommini@gmail.com>
2026-05-22 20:08:42 +0530

    this is my commit

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ##### Special Vars #####
-person="Lokesh"
-echo " this is $person, started learning shell scripting"
+person=$1
+read  "Enter your name: " person
+echo " this is $1, started learning shell scripting"
 
 echo "All variables passed to script: $@"
 echo "Number of variables passed: $#"
