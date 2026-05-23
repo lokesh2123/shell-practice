1ee6373ddaa7ddb7139a9a80998eb10cae1b8f98
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 15:00:36 +0530

    this is my commit

--- assignment5.sh
+++ assignment5.sh
@@ -6,14 +6,29 @@
 #Runs ping -c1 google.com and checks — print Host reachable or Host not reachable
 #At the end, exit the script with code 0 if all commands succeeded, or 1 if any failed
 
-echo "Runs : $(ls /tmp)" 
-echo "Runs : $(ls /hdvhvdv)"
-echo "Runs : $(ping -c 'google.com')"
-
-
+ls /tmp
 if [ $? -eq 0 ]; then
-    echo "Command executed successfully"
-else 
-    echo "Command execution failed"
-fi
- 
+    echo "ls /tmp ... SUCCESS"
+else
+    echo "ls /tmp ... FAILURE"
+    exit 1
+fi          
+
+ls /fakedir
+if [ $? -eq 0 ]; then
+    echo "ls /fakedir ... SUCCESS"
+else
+    echo "ls /fakedir ... FAILURE"
+    exit 1
+fi  
+
+ping -c1 google.com
+if [ $? -eq 0 ]; then
+    echo "ping google.com ... Host reachable"
+else
+    echo "ping google.com ... Host not reachable"
+    exit 1
+fi  
+
+echo "All commands succeeded ... exiting with code 0"
+exit 0                          
