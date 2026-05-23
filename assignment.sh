84d17a1bcdf6626d718b3110a7a7245dad3a23d1
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 13:31:29 +0530

    this is my commit

--- assignment.sh
+++ assignment.sh
@@ -4,10 +4,13 @@ echo "This is my first script"
 echo "Hello world"
 
 DATE=$(date +"%Y-%m-%d %H:%M:%S %Z")
-sleep 1
 echo "Current date and time is $DATE"
 
 
 #Prints the name of the user running the script
 USER=$(whoami)
 echo "The user running this script is $USER"
+
+#Prints the directory the script is running from
+DIR=$(pwd)
+echo "The current working directory is $DIR"
