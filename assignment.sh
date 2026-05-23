a23c4e9fe8a17900fefe17c19fbe9a930fb7dd6f
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 13:29:43 +0530

    this is my commit

--- assignment.sh
+++ assignment.sh
@@ -6,3 +6,8 @@ echo "Hello world"
 DATE=$(date +"%Y-%m-%d %H:%M:%S %Z")
 sleep 1
 echo "Current date and time is $DATE"
+
+
+#Prints the name of the user running the script
+USER=$(whoami)
+echo "The user running this script is $USER"
