e22913561e62837be58946ccfafcbcf25a2a5230
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 13:37:00 +0530

    this is my commit

--- assignment2.sh
+++ assignment2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+#Store your name, age, and city in variables
 NAME=Lokesh    
 AGE=29
 CITY=KOTALLA
@@ -7,3 +9,7 @@ CITY=KOTALLA
 echo "My name is $NAME"
 echo "I am $AGE years old" 
 echo "and I live in $CITY"
+
+#Print a sentence using all three variables
+
+echo "My name is $NAME, I am $AGE years old and I live in $CITY"
