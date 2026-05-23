b7011ea56f36b2222d2e86fe0a9fd8b90313df0b
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 13:46:20 +0530

    this is my commit

--- assignment3.sh
+++ assignment3.sh
@@ -3,10 +3,10 @@
 #Print: User <username> is from <country>
 
 USERNAME=$(whoami)
-COUNTRY=$((india))
+COUNTRY="India"
 
 echo "User $USERNAME is from $COUNTRY"
 
 #PPrint total number of arguments passed
 
-echo "Total number of arguments passed: $#"
+echo "Total number of arguments passed: $@"
