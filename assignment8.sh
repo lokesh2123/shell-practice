1facbc5b89f40a0ddcbcf1b1455bd2ae4a160fe9
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 21:33:09 +0530

    this is my commit

--- assignment8.sh
+++ assignment8.sh
@@ -5,7 +5,7 @@
 #If it is Monday, print Start of the week. Let's go!
 #For any other day, print Weekday. Go to school/work.
 
-DAY=$(date +%A)         
+DAY=$1       
 if [ "$DAY" == "Saturday" ] || [ "$DAY" == "Sunday" ]; then
     echo "Weekend! Enjoy your holiday."
 elif [ "$DAY" == "Monday" ]; then
