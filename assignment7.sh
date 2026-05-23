bcf9b7e51bc825425b134cf43fc3d76b82b50106
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 21:24:33 +0530

    this is my commit

---
+++ assignment7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Accepts a number as a command line argument
+#Validates that the argument is provided — exit with an error if missing
+#Checks whether the number is a prime number or not
+#Prints the result
+
+NUMBER=$1
+if [ -z "$NUMBER" ]; then
+    echo "Error: No number provided. Please provide a number as a command line argument."
+    exit 1
+fi
