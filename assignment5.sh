364658782d1c9e7c8f781d9af77240f1f8096707
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 14:54:32 +0530

    this is my commit

--- assignment5.sh
+++ assignment5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Runs : $(ls /tmp) and check if it is successful or not" 
-echo "Runs : $(ls /hdvhvdv) and check if it is successful or not"
-echo "Runs : $(ping -c google .com) and check if it is successful or not"
+echo "Runs : $((ls /tmp)) and check if it is successful or not" 
+echo "Runs : $((ls /hdvhvdv)) and check if it is successful or not"
+echo "Runs : $((ping -c google.com)) and check if it is successful or not"
 
 
 [ $? -eq 0 ] then 
