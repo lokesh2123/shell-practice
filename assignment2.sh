b0679ac81ee545f23dfe1c2e4307da7a9b5ec76a
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 13:38:17 +0530

    this is my commit

--- assignment2.sh
+++ assignment2.sh
@@ -13,3 +13,7 @@ echo "and I live in $CITY"
 #Print a sentence using all three variables
 
 echo "My name is $NAME, I am $AGE years old and I live in $CITY"
+
+#Calculate and print how many years until you turn 60
+YEARS_TO_60=$((60 - AGE))
+echo "I will turn 60 in $YEARS_TO_60 years"
