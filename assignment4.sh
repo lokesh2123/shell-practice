4d427bcb007f75f7bc50008aab3af3842a1adef3
Lokesh <Lokeshkommini@gmail.com>
2026-05-23 14:36:08 +0530

    this is my commit

--- assignment4.sh
+++ assignment4.sh
@@ -5,5 +5,8 @@ USER=$(whoami)
 echo "The user running this script is $USER"
 
 
+#Print the home directory
+echo "The home directory of the user is $HOME"
+
 #Print the current directory
-echo " Print the current directory : $HOME"
+echo " Print the current directory : $PWD"
