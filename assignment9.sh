f5acee249f1c64afe9f06cbead37503ee959832b
Lokesh <Lokeshkommini@gmail.com>
2026-05-24 21:59:14 +0530

    this is my commit

--- assignment9.sh
+++ assignment9.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-#Checks if the script is run as root — if not, print an error and exit with code 1
-
-if [ "$EUID" -ne 0 ]; then
-    echo "Error: This script must be run as root."
-    exit 1
-fi  
-
-#Accepts a package name as argument (e.g., nginx)
-
-if [ -z "$1" ]; then
-    echo "Error: No package name provided. Please provide a package name as an argument."
-    exit 1
-fi
+#Checks if the script is run as root — if not, print an error and exit with code 
 
 #Checks if the package is already installed — if yes, print nginx is already installed and exit
 dnf list installed MySQL
